462a88753afa1a6038893279e079943030c645b5
Jon Mikoš <atletjm@gmail.com>
2018-05-03 14:11:10 +0200

    Very productive

--- graphs.py
+++ graphs.py
@@ -1,4 +1,7 @@
 class DirectedGraph:
+
+	splitSign = '-->' 
+
 	def __init__(self, vertices, arrows):
 		self._V = vertices
 		self._A = arrows
@@ -9,7 +12,12 @@ class DirectedGraph:
 
 	@vertices.setter
 	def vertices(self, values):
-		self.arrows = [arrow for arrow in self.arrows if arrow[0] in values and arrow[1] in values]
+		temp = {}
+		for arrow, val in self.arrows.items():
+			head, tail = arrow.split(self.splitSign)
+			if head in values and tail in values:
+				temp[self._makeEdge(head, tail)] = val
+		self._A = temp
 		self._V = values
 
 	@property
@@ -18,7 +26,12 @@ class DirectedGraph:
 
 	@arrows.setter
 	def arrows(self, values):
-		self._A = values
+		temp = {}
+		for arrow, val in self.arrows.items():
+			head, tail = arrow.split(self.splitSign)
+			if head in self.vertices and tail in self.vertices:
+				temp[self._makeEdge(head, tail)] = val
+		self._A = temp
 
 	@property
 	def size(self):
@@ -37,13 +50,15 @@ class DirectedGraph:
 		return self._makeEdge(start, end) in self.arrows
 
 	def _makeEdge(self, start, end):
-		return ''.join([start, end])
+		return self.splitSign.join([start, end])
 
 	def __repr__(self):
-		return '{} with vertices {} and edges {}'.format(self.__class__.__name__, ', '.join(self.vertices), ', '.join(self.arrows))
+		return '{} with vertices {} and arrows {}'.format(self.__class__.__name__, ', '.join(self.vertices), ', '.join(self.arrows))
 
-a = DirectedGraph(['A', 'B', 'C', 'D', 'E'], {'AB':1, 'CD':1, 'AE':1, 'AD':1, 'BA':1, 'BB':2, 'BE':4, 'DC':2, 'EB':2})
+a = DirectedGraph(['A', 'B', 'C', 'D', 'E'], {'A-->B':1, 'C-->D':1, 'A-->E':1, 'A-->D':1, 'B-->A':1, 'B-->B':2, 'B-->E':4, 'D-->C':2, 'E-->B':2})
 a.vertices = ['A', 'B', 'C']
+a.arrows = {'A-->D':3, 'A-->A':10, 'B-->C':4, 'A-->B': 1}
 for row in a.adjacencyMatrix:
 	print(str(row))
-print(a, a.vertices)
+print(a)
+print(a.vertices, a.arrows)
